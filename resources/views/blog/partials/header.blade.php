9876de9ab14782945443087e32a4a2b34231655b
Frank <frank.qf.cn@gmail.com>
2015-10-21 16:13:47 +0800

    fix navigation issue

--- resources/views/blog/partials/header.blade.php
+++ resources/views/blog/partials/header.blade.php
@@ -1,9 +1,9 @@
 <header id="header">
     <div id="head" class="parallax" parallax-speed="1">
-        <h1 id="logo" class="text-center">
+        <h1 id="logo" class="text-center" style="margin-top: 75px;">
             <img class="img-circle" src="/images/uploads/blog/guy.jpg" alt="">
             <span class="title"><a href="/blog/" class="expend">{{ trans('messages.Netcafe')}} - {{ trans('messages.Our Blogs') }}</a></span>
-            <span class="tagline"><i>{{ trans('messages.What we have done so far') }}</i></span>
+            @if(false)<span class="tagline"><i>{{ trans('messages.What we have done so far') }}</i></span>@endif
         </h1>
     </div>
 </header>
