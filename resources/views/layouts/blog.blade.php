98ce6038f8378682f83a61ee41b6905936749b2c
Frank <frank.qf.cn@gmail.com>
2015-10-22 12:06:05 +0800

    header

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
 	@include('blog.partials.head')
 </head>
 <body>
+<!--[if lt IE 7]>
+    <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+<![endif]-->
 @include('blog.partials.page-nav')
 @yield('page-header')
 @include('blog.partials.header')
